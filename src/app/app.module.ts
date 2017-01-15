822baf40d61b7914b9f33065241d9b0266ff7597
Basavaraj <basavarajnb@gmail.com>
2017-01-15 14:17:42 +0530

    Startup 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './subapp/home/home.component';
 import { AboutComponent } from './subapp/about/about.component';
+import { GalleryComponent } from './subapp/gallery/gallery.component';
+import { CeremonyComponent } from './subapp/ceremony/ceremony.component';
+import { RsvpComponent } from './subapp/rsvp/rsvp.component';
+import { WeddingcardComponent } from './subapp/weddingcard/weddingcard.component';
+import { RegistryComponent } from './subapp/registry/registry.component';
 import { ApiService } from './shared';
 import { routing } from './app.routing';
 
@@ -21,7 +26,12 @@ import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    GalleryComponent,
+    CeremonyComponent,
+    RsvpComponent,
+    WeddingcardComponent,
+    RegistryComponent
   ],
   providers: [
     ApiService
